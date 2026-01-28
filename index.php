98f84c463a4e7e4ea6c8c29f9b773366d2654921
Marius J. <57361575+MariusJam@users.noreply.github.com>
2026-01-28 14:49:16 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,11 +5,6 @@ require_once __DIR__ . '/BuggyClass.php';
 
 echo "Accessing constant...\n";
 
-// This access should not trigger the SIGSEGV if the class was preloaded
-$const = \App\BuggyClass::TEST_CONST_STRING;
-echo "Success: " . $const . "\n";
-
 // This access should trigger the SIGSEGV if the class was preloaded
-echo "Next should crash \n";
 $const = \App\BuggyClass::TEST_CONST;
 echo "Success: " . ($const)() . "\n";
